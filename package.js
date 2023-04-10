7c46fdece69c65c5d03113f8c536efe3ed441c76
SACHIBOT26 <108348574+SACHIBOT26@users.noreply.github.com>
2023-04-10 16:06:15 +0530

    Add files via upload

--- package.js
+++ package.js
@@ -3,10 +3,21 @@
   "version": "1.0.0",
   "description": "Script to automatically restart a Heroku dyno",
   "main": "heroku.js",
-  "dependencies": {
-    "heroku-client": "^5.2.2"
-  },
+
   "scripts": {
     "start": "node heroku.js"
   }
+
+  "dependencies": {
+    "heroku-client": "^5.2.2"
+   "express": "^4.17.1"
+  },
+  "engines": {
+    "node": "14.x"
+  },
+  "buildpacks": [
+    {
+      "url": "heroku/nodejs"
+    }
+  ]
 }
