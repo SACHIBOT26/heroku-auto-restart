a37745161788619a25d306882f4cab31654bf4d1
SACHIBOT26 <108348574+SACHIBOT26@users.noreply.github.com>
2023-04-10 16:16:56 +0530

    Update package.js

--- package.js
+++ package.js
@@ -1,23 +1,41 @@
 {
+
   "name": "heroku-auto-restart",
+
   "version": "1.0.0",
+
   "description": "Script to automatically restart a Heroku dyno",
+
   "main": "heroku.js",
 
   "scripts": {
+
     "start": "node heroku.js"
-  }
+
+  },
 
   "dependencies": {
-    "heroku-client": "^5.2.2"
-   "express": "^4.17.1"
+
+    "heroku-client": "^5.2.2",
+
+    "express": "^4.17.1"
+
   },
+
   "engines": {
+
     "node": "14.x"
+
   },
+
   "buildpacks": [
+
     {
+
       "url": "heroku/nodejs"
+
     }
+
   ]
+
 }
